dc5d8fff7340e2a73a87131d24b1e905facbe693
andrecp <andreprado88@gmail.com>
2015-07-18 17:58:03 +0100

    admin: adding questions and choices and customisations as for tutorial part 02

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,29 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    # Admin view attributes.
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text

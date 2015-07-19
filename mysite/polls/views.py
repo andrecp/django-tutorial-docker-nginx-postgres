e84811ac702d72e61729c7b211c72cb2812f2958
andrecp <andreprado88@gmail.com>
2015-07-19 12:09:00 +0100

    django tutorial: part 3 rendering a response with templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Question
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)

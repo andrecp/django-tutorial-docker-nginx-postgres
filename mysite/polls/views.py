fda3c34191574867cd07b8c5006f7d1ff6188f52
andrecp <andreprado88@gmail.com>
2015-07-19 12:44:48 +0100

    django tutorial: end of part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from polls.models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
@@ -11,7 +11,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):

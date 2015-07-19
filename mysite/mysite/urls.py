3e716b6726437b5120699afe4fb1c51486d23013
andrecp <andreprado88@gmail.com>
2015-07-19 11:57:04 +0100

    django tutorial: part 3 creating views.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
